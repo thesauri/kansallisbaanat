41af4c30ace748788535c6f31c30cb2c26abc72d
Walter Berggren <walter.berggren@gmail.com>
2020-06-27 14:14:31 +0300

    Center map around Finland

--- src/components/map.js
+++ src/components/map.js
@@ -4,6 +4,7 @@ import "./map.css"
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoidGhlc2F1cmkiLCJhIjoiY2tieGgzMWRwMGk3bDJxbGpya3B0ZjJ0MSJ9.675WOKVjzYQypCJLa0TFrQ"
+const CENTER_OF_FINLAND = [25, 65.3]
 
 const Map = () => {
   const mapElement = useRef(null)
@@ -11,8 +12,8 @@ const Map = () => {
     const map = new mapboxgl.Map({
       container: mapElement.current,
       style: "mapbox://styles/mapbox/streets-v11",
-      center: [0, 0],
-      zoom: 2,
+      center: CENTER_OF_FINLAND,
+      zoom: 4,
     })
   }, [])
   return <div ref={mapElement} className="map-container"></div>
