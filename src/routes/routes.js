14c1448d5a731168e44a639cbba1121a14593416
Walter Berggren <walter.berggren@gmail.com>
2020-06-28 17:00:41 +0300

    Add all but Utsjoki and Åland routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,21 @@
 import { MUROLEEN_KANAVA_KAPEE_TERALAHTI } from "./muroleen-kanava-kapee-teralahti"
+import { SALO_RASEBORG } from "./salo-raseborg"
+import { KRISTINA_PUUMALA } from "./kristina-puumala"
+import { VAAKSY_JYVASKYLA } from "./vaaksy-jyvaskyla"
+import { MERIKARVIA_BJORNEBORG } from "./merikarvia-bjorneborg"
+import { OULUJARVI } from "./oulujarvi"
+import { MUHOS_HAILUOTO } from "./muhos-hailuoto"
+import { KEMIJARVI_KUUSAMO } from "./kemijarvi-kuusamo"
 
-const { SALO_RASEBORG } = require("./salo-raseborg")
-
-const routes = [SALO_RASEBORG, MUROLEEN_KANAVA_KAPEE_TERALAHTI]
+const routes = [
+  SALO_RASEBORG,
+  MUROLEEN_KANAVA_KAPEE_TERALAHTI,
+  KRISTINA_PUUMALA,
+  VAAKSY_JYVASKYLA,
+  MERIKARVIA_BJORNEBORG,
+  OULUJARVI,
+  MUHOS_HAILUOTO,
+  KEMIJARVI_KUUSAMO,
+]
 
 export default routes
